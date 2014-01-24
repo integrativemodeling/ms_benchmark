eed7a1bde8b05e87777ff9c9161cf43b16312c55
Ben Webb <ben@salilab.org>
2014-01-24 09:28:42 -0800

    Show a wider range of model connectivity scores.

--- test/test.py
+++ test/test.py
@@ -12,39 +12,39 @@ class Tests(unittest.TestCase):
     def test_aabc(self):
         """Test AABC example"""
         os.chdir(os.path.join(TOPDIR, 'tAABC'))
-        self.run_scripts(min_models=9900, min_perfect=2500)
+        self.run_scripts(min_models=9900, min_threshold=2500)
 
     def test_abcd(self):
         """Test ABCD example"""
         os.chdir(os.path.join(TOPDIR, 'tABCD'))
-        self.run_scripts(min_models=9900, min_perfect=2500)
+        self.run_scripts(min_models=9900, min_threshold=2500)
 
     def test_abcde(self):
         """Test ABCDE example"""
         os.chdir(os.path.join(TOPDIR, 'tABCDE'))
-        self.run_scripts(min_models=9900, min_perfect=2500)
+        self.run_scripts(min_models=9900, min_threshold=2500)
 
     def test_aabbc(self):
         """Test AABBC example"""
         os.chdir(os.path.join(TOPDIR, 'tAABBC'))
-        self.run_scripts(min_models=9900, min_perfect=2500)
+        self.run_scripts(min_models=9900, min_threshold=2500)
 
     def test_11mer(self):
         """Test 11mer example"""
         os.chdir(os.path.join(TOPDIR, 't-11mer'))
-        self.run_scripts(min_models=4900, min_perfect=1000)
+        self.run_scripts(min_models=4900, min_threshold=1000)
 
     def test_clamp_7mer(self):
         """Test clamp loader 7mer example"""
         os.chdir(os.path.join(TOPDIR, 'clamp-loader', 'clamp-loader-7mer'))
-        self.run_scripts(min_models=9900, min_perfect=2500)
+        self.run_scripts(min_models=9900, min_threshold=2500)
 
     def test_clamp_5mer(self):
         """Test clamp loader 5mer example"""
         os.chdir(os.path.join(TOPDIR, 'clamp-loader', 'clamp-loader-5mer'))
-        self.run_scripts(min_models=9900, min_perfect=2500)
+        self.run_scripts(min_models=9900, min_threshold=2500)
 
-    def run_scripts(self, min_models, min_perfect):
+    def run_scripts(self, min_models, min_threshold, threshold=1):
         # Test modeling script
         p = subprocess.check_call(['./ms_cg.py'])
         # Make sure models and clusters were produced
@@ -60,13 +60,14 @@ class Tests(unittest.TestCase):
         val = [float(x) for x in f.rstrip('\r\n').split(' ')]
         num = len(val)
         self.assert_(num > min_models, "Only %d models were checked" % num)
-        num_perfect = len([x for x in val if x < 1.0])
-        num_2 = len([x for x in val if x < 2.0])
-        num_3 = len([x for x in val if x < 3.0])
-        num_4 = len([x for x in val if x < 4.0])
-        self.assert_(num_perfect > min_perfect,
-                     "Only %d models were perfect (%d < 2.0, "
-                     "%d < 3.0, %d < 4.0)" % (num_perfect, num_2, num_3, num_4))
+        num_threshold = {}
+        for t in range(1, 30):
+            num_threshold[t] = len([x for x in val if x < t])
+        self.assert_(num_threshold[threshold] > min_threshold,
+                     "Only %d models were below %d (%s)" % \
+                     num_threshold[threshold], threshold,
+                     ", ".join("%d < %d" % (num_threshold[x], x) \
+                                            for x in range(1, 30)))
 
 if __name__ == '__main__':
     unittest.main()
