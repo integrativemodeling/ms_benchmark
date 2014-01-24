7e84e3db82547d70c2f357aa7ff7df83c36a3e9a
Ben Webb <ben@salilab.org>
2014-01-23 16:10:48 -0800

    Add basic test case.

---
+++ test/test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import unittest
+import os
+import glob
+
+class Tests(unittest.TestCase):
+    def test_aabc(self):
+        """Test AABC example"""
+        os.chdir(os.path.join(os.path.dirname(sys.argv[0]), '..', 'tAABC'))
+        self.run_scripts(min_models=9900, min_perfect=2500)
+
+    def test_abcd(self):
+        """Test ABCD example"""
+        os.chdir(os.path.join(os.path.dirname(sys.argv[0]), '..', 'tABCD'))
+        self.run_scripts(min_models=9900, min_perfect=2500)
+
+    def test_aabbc(self):
+        """Test AABBC example"""
+        os.chdir(os.path.join(os.path.dirname(sys.argv[0]), '..', 'tAABBC'))
+        self.run_scripts(min_models=9900, min_perfect=2500)
+
+    def run_scripts(self, min_models, min_perfect):
+        # Test modeling script
+        p = subprocess.check_call(['./ms_cg.py'])
+        # Make sure models and clusters were produced
+        num = len(glob.glob('output/models/configuration.*.pym'))
+        self.assert_(num > min_models, "Only %d models were produced" % num)
+        self.assert_(len(glob.glob('output/models/cluster.*.pym'), 10)
+        # Make mfj files
+        p = subprocess.check_call(['./pym2tcl.py'])
+        p = subprocess.check_call(['./tcl2mfj.py'])
+        # Check the models against known connectivity
+        p = subprocess.check_call(['./score_connect.py'])
+        f = open('output/score-connect.txt').readlines()[1]
+        val = [float(x) for x in f.rstrip('\r\n').split(' ')]
+        num = len(val)
+        self.assert_(num > min_models, "Only %d models were checked" % num)
+        num_perfect = len([x for x in val if x < 1.0])
+        num_2 = len([x for x in val if x < 2.0])
+        num_3 = len([x for x in val if x < 3.0])
+        num_4 = len([x for x in val if x < 4.0])
+        self.assert_(num_perfect > min_perfect,
+                     "Only %d models were perfect (%d < 2.0, "
+                     "%d < 3.0, %d < 4.0)" % (num_perfect, num_2, num_3, num_4))
+
+if __name__ == '__main__':
+    unittest.main()
